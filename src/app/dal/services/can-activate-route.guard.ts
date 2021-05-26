9690009d9d5cc138140316f8bc96308493f953ae
Nikhil Kumar <nikhil.kumar5@publicissapient.com>
2021-05-26 19:16:30 +0530

    using urltree and enabling prod build

--- src/app/dal/services/can-activate-route.guard.ts
+++ src/app/dal/services/can-activate-route.guard.ts
@@ -1,13 +1,22 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, Router } from "@angular/router";
+import { 
+  ActivatedRouteSnapshot, 
+  CanActivate,
+  Router, 
+  RouterStateSnapshot, 
+  UrlTree 
+} from "@angular/router";
+import { Observable } from "rxjs";
 import { DataService } from "./data-service";
 
 @Injectable({providedIn: 'root'})
 
 export class CanActivateRouteGuard implements CanActivate{
   constructor(private router: Router) {}
-  canActivate(): boolean{
-    if(!DataService.errorOccured) this.router.navigate(['/home'])
-    return DataService.errorOccured;
+  canActivate(
+    route:ActivatedRouteSnapshot,
+    routerState: RouterStateSnapshot
+  ): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree>{
+    return !DataService.errorOccured ? this.router.createUrlTree(['/home']) : DataService.errorOccured;
   }
 }
